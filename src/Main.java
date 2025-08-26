2e4a8cbc25ac7cc189adab7a81716000c80c28d5
xeneizee12 <niconevach7@gmail.com>
2025-08-26 09:35:06 -0300

    v0

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		JOptionPane.showMessageDialog(null, "Hola soy ghami");
+		JOptionPane.showMessageDialog(null, "Hola soy Nico");
 		
 	}
 }
